a220bfeb173cc91df9c49b34735570779d3fb77c
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2023-03-25 16:06:13 +0400

    Filter section

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 // First Slide start
-const prew = document.querySelector(".prew");
-const next = document.querySelector(".next");
-const firstSlide = document.querySelector(".slide_first");
-const firstSlideContent = document.querySelector(".firstSlideWritting");
 
-let firstSlideArr = ["h1_hero1.jpg", "h1_hero2.jpg"];
-let index = 0;
+$(document).ready(function () {
+  $(".btn").click(function () {
+    $(".carusel").fadeToggle(150);
+    $(".carusel2").fadeToggle(150);
+  });
+  let scroll=0;
+  $(".filter_btn_next").click(()=>{
+    scroll+=500
+    $(".filter_boxes").scrollLeft(scroll)
+  })
+  $(".filter_btn_prew").click(()=>{
+    scroll-=500
+    $(".filter_boxes").scrollLeft(scroll)
+  })
 
-next.addEventListener("click", () => {
-  firstSlide.setAttribute("src", `./images/${[firstSlideArr[index]]}`);
-  if (index == firstSlideArr.length - 1) {
-    firstSlideContent.classList.toggle("slideWrittingPart2")
-    index = 0;
-  } else {
-    index++;
-  }
-});
-prew.addEventListener("click", () => {
-  firstSlide.setAttribute("src", `./images/${firstSlideArr[index]}`);
-  if (index == 0) {
-    index = firstSlideArr.length - 1;
-  } else {
-    index--;
-  }
+  
 });
 
 // First Slide end
+
+//modal start
+
+const modal = document.querySelector(".modal");
+let loginButton = document.querySelector(".loginButton");
+
+loginButton.addEventListener("click", () => {
+  modal.classList.toggle("modalClick");
+});
+
+
+let products=[
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest1.jpg",category:"men"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest2.jpg",category:"women"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest3.jpg",category:"baby"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest1.jpg",category:"men"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest2.jpg",category:"women"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest3.jpg",category:"baby"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest1.jpg",category:"men"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest2.jpg",category:"women"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest1.jpg",category:"men"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest3.jpg",category:"baby"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest2.jpg",category:"women"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest3.jpg",category:"baby"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest2.jpg",category:"women"},
+  {name:"Cashmere Tank + Bag",price:99,oldPrice:120,pic:"./images/latest3.jpg",category:"baby"},
+];
+products.forEach(product => {
+  let boxes=document.querySelector(".filter_boxes")
+  boxes.innerHTML+=`   
+  <div class="filter_box">
+    <img src="${product.pic}" alt="">
+    <p class="about_filterBox">${product.name}</p>
+    <p class="price_filterBox">$${product.price} <del>$${product.oldPrice}</del></p>
+  </div>`
+});
